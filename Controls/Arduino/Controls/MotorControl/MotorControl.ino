1386aeb40f947397c0ecfe1518107c74aba59e2e
asshole3214 <limpinpin123@gmail.com>
2021-05-31 23:18:05 +0800

    Changes made
    -> Added motorTest.ino code for general arduino code
    
    -> Added TeensyHardwareTest.ino for teensy teesting
    
     -> Made changes to MotorControl.ino
         -> MotorControls can now spin the
              all 4 motors

--- Controls/Arduino/Controls/MotorControl/MotorControl.ino
+++ Controls/Arduino/Controls/MotorControl/MotorControl.ino
@@ -1,87 +1,167 @@
 #include <Servo.h>
 
-enum motorState{Off , Idle , Running};
-struct PropelsionMotor
+#define MOTORSIZE 5 // Change this if this physical motor count increases
+#define M1_PIN 23
+#define M2_PIN 10
+#define M3_PIN 14
+#define M4_PIN 22
+#define M5_PIN 15
+
+
+struct Motor 
 {
-  int pinNo;
-  int armingAngle;
-  int speedFreq ; 
-  motorState motorState = Off; 
-  Servo motor; 
+   int pinNo;
+   int armingFreq; 
+   Servo esc; 
 };
 
-PropelsionMotor motors[4];
+Motor motors[MOTORSIZE];
+int motorPins [MOTORSIZE] = {M1_PIN,M2_PIN,M3_PIN,M4_PIN,M5_PIN};
+int armingFreq[MOTORSIZE] = {1480,1480,1480,1480,1480};
+int generalArmingFreq = 1480;
+Servo esc;
 
-
-//=========Function Header=====================
-//void motorSetParam(PropelsionMotor motor ,int _pinNo ,int _armingAngle , int _speedFreq );
-//void initMotor(); 
-//void motorSetSpeed(PropelsionMotor motor);
-
-
-
-//===========TEMP HOLDING VALUE================
-int value=map(89,0,180,1000,2000);
-
-//=========== MAIN INIT LOOP===================
-void setup() {
-  // put your setup code here, to run once:
-  
-  /*motors[0].pinNo = 11 ;
-  motors[0].armingAngle = 89;
-  motors[0].speedFreq = map(89,0,180,1000,2000);*/
-
+void setup() 
+{
   Serial.begin(9600);
-  motorSetParam(motors[0] ,11,89, map(89,0,180,1000,2000) );
-  //TODO: switch to this once testing for 5 motors 
-  /*for(int i =0 ; i<=motors.length(); i++)
-  {
-    initMotor(motors[i]);
-  }*/
-  
-  initMotor(motors[0]);
+  armAll();
 }
 
-//============== MAIN CONTROL LOOP ===============
 void loop() 
 {
-  //TODO : IMPLEMENT TIME SLICING CODE HERE : 
- motorSetSpeed(motors[0],value);
+  //test();
+  testSingle(motors[1]);
 }
 
-//====================INIT CODE===========================================
-void motorSetParam(PropelsionMotor _motor ,int _pinNo ,int _armingAngle , int _speedFreq )
+//============================INIT CODE ==================================
+void armAll()
 {
-  _motor.pinNo = _pinNo;
-  _motor.motor.attach(_pinNo);
-  _motor.armingAngle = _armingAngle;
-  _motor.speedFreq = _speedFreq;
-}
-void initMotor(PropelsionMotor _motor)
-{
-  delay(2000); // allow for ESC side(pre built) code to settle
-  Serial.println("initMotor : Motor Arming");
-  _motor.motor.write(_motor.armingAngle);
-  delay(500);
-  Serial.println("initMotor : Motor Armed ");
+  Serial.println("Arming Motors");
+  delay(5000);
+  for(int i = 0 ; i<MOTORSIZE ; i++)
+  {
+    motors[i].pinNo = motorPins[i];
+    motors[i].armingFreq = armingFreq[i];
+    motors[i].esc.attach(motorPins[i]);
+
+    motors[i].esc.writeMicroseconds(motors[i].armingFreq);
+    ///BLOCKING CODE 
+    delay(3000);
+    ///BLOCKING CODE 
+    Serial.print(motors[i].pinNo);Serial.print(" ");Serial.println(motors[i].armingFreq);
+  }
+  Serial.println("MOTORS ARMED !!!");
 }
 
-//============================COMMS CODE ==================================
+///============================COMMS CODE ==================================
 void serialEvent()
 {
   //!WARNING : TEMP CODE REPLACE WITH LOOK UP FUCNTION FOR DYNAMIC COMMS METHOD
   String readValue = Serial.readString(); 
   //readValue.flush();
-  value = readValue.toInt(); 
+  int value = readValue.toInt(); 
   Serial.print("ReadValue : ");Serial.println(readValue);
   Serial.print("ActualValue : ");Serial.println(value);
 }
+//TODO : Add ROSSERIAL CODE HERE
 
-//============================MOTOR CONTROL CODE ==================================
-void motorSetSpeed(PropelsionMotor _motor,int _speedFreq)
+///============================MOTOR CONTROL CODE ==================================
+void motorSetSpeedAllPercentage(int _speedPercentage [])
+{
+  //TAKE NOTE : MAKE SURE THAT _speedPercentage ARRAY SIZE IS EQUALS TO MOTORSIZE
+  for(int i =0 ;i<MOTORSIZE ; i++)
+  {
+    motors[i].esc.writeMicroseconds(freqConversion(_speedPercentage[i]));  
+    Serial.print("Motor Speed change : Motor : "); Serial.print(i); Serial.print(" speedPercentage : ");Serial.println(_speedPercentage[i]);
+  }
+}
+
+void motorSetSpeedFreq(Motor _motor,int _speedFreq)
+{
+  //TODO:SCALE THIS UP TO 4 MOTORS FOR EASE OF CONTROL
+  _motor.esc.writeMicroseconds(_speedFreq);
+  Serial.print("Motor Speed change : ");Serial.println(_speedFreq);
+}
+void motorSetSpeedPercentage(Motor _motor,int _speedPercentage)
 {
   //TODO:SCALE THIS UP TO 4 MOTORS FOR EASE OF CONTROL 
-  _motor.speedFreq  = _speedFreq;
-  _motor.motor.writeMicroseconds(_speedFreq);
-  Serial.print("Motor Speed change : ");Serial.println(_motor.speedFreq);
+  _motor.esc.writeMicroseconds(freqConversion(_speedPercentage));
+  Serial.print("Motor Speed change : ");Serial.println(_speedPercentage);
+}
+///============================COMPUTATION CODE ==================================
+int freqConversion(int speedPercentage)
+{
+  if(speedPercentage >= 0)
+  {
+    //CW code : 
+    int freq = map(speedPercentage,0,100,generalArmingFreq,1900 );
+    return freq;
+  }
+  else if(speedPercentage <=0 )
+  {
+    //ACW code : 
+    int freq = map(-speedPercentage,0,100,1100,generalArmingFreq);
+    return freq;
+  }
+  else
+  {
+    //default code 
+    Serial.print("freq :Invalid input : speedPercentage : ");Serial.println(speedPercentage);
+  }
+}
+
+///============================TESTING CODE ==================================
+void testSingle(Motor motor)
+{
+  Serial.println("spinning cw");
+  int delayTime = 1000;
+   for(int i =0 ; i<100 ; i+=10 ) 
+   {
+     Serial.println(i);
+     motorSetSpeedPercentage(motor , i);
+     delay(delayTime);
+   }
+   for(int i =100  ; i>0 ; i-=10 ) 
+   {
+    Serial.println(i);
+    motorSetSpeedPercentage(motor , i);
+    delay(delayTime);
+   }
+}
+void test()
+{
+   Serial.println("spinning cw");
+   int delayTime = 1000;
+   for(int i =0  ; i<100 ; i+=1 ) 
+   {
+     Serial.println(i);
+     int speedPercentage[] = {i,i,i,i,i};
+     motorSetSpeedAllPercentage(speedPercentage);
+
+     delay(delayTime);
+   }
+   for(int i =100  ; i>0 ; i-=1 ) 
+   {
+     Serial.println(i);
+     int speedPercentage[] = {i,i,i,i,i}; 
+     motorSetSpeedAllPercentage(speedPercentage);
+     
+     delay(delayTime);
+   }
+   for(int i =0  ; i>-100 ; i-=1 ) 
+   {
+     Serial.println(i);
+     int speedPercentage[] = {i,i,i,i,i};
+     motorSetSpeedAllPercentage(speedPercentage);
+
+     delay(delayTime);
+   }
+   for(int i =-100  ; i<0 ; i+=1 ) 
+   {
+     Serial.println(i);
+     int speedPercentage[] = {i,i,i,i,i}; 
+     motorSetSpeedAllPercentage(speedPercentage);
+     
+     delay(delayTime);
+   }
 }
--- Controls/Arduino/Test/MORETESTKILLME/MORETESTKILLME.ino
+++ Controls/Arduino/Test/MORETESTKILLME/MORETESTKILLME.ino
@@ -3,9 +3,11 @@
 
 Servo esc;
 
+// Currently attached pins = 23,22,21,20,10 
+
 void setup()
 {
-  esc.attach(11);
+  esc.attach(23);
 /*
 As you can see - I am not arming the ESC here.
 For some reason, the ESC is now on a persistently armed mode now.
@@ -27,12 +29,14 @@ void loop()
   // Start at Servo Degree 70 and go till just above neutral.
   // Note, Neutral is at 94 for this ESC.
   // This will run it in one direction.
-  for(i=70;i<95;i++)
+  for(i=80;i<95;i++)
   {
     esc.write(i);
     delay(500);
     Serial.println(i);
   }
+  
+  Serial.println("Armed");
 //Run for 2 seconds at moderately high speed (70).
   esc.write(70); delay(2000);
 // Apply Brakes for 40 millisecs.
@@ -45,6 +49,7 @@ void loop()
     esc.write(i);
     delay(200);
   }
+  Serial.println("disarmed");
 // Apply brakes again - to change directions for 40 millisecs.
   esc.write(70); delay(40);
 }
